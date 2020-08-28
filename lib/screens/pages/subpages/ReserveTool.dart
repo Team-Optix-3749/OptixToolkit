ed5c7d6286fbc0c588f2c81585c2cb95215a00f1
Raadwan Masum <piraadwan@gmail.com>
2020-08-28 13:02:01 -0700

    add boilerplate date and time picker

--- lib/screens/pages/subpages/ReserveTool.dart
+++ lib/screens/pages/subpages/ReserveTool.dart
@@ -20,6 +20,7 @@ class _ReserveToolState extends State<ReserveTool> {
   final Color gray = Color(0xff3A3D41);
   final Color subtleGray = Color(0xffcccccc);
   String dropdownValue = "Select a Tool";
+  String _time = "Not set";
 
   @override
   Widget build(BuildContext context) {
@@ -113,6 +114,82 @@ class _ReserveToolState extends State<ReserveTool> {
                           ),
                         ),
                       ),
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        elevation: 4.0,
+                        onPressed: () {
+                          DatePicker.showTimePicker(context,
+                              theme: DatePickerTheme(
+                                containerHeight: 210.0,
+                              ),
+                              showTitleActions: true, onConfirm: (time) {
+                            print('confirm $time');
+                            _time =
+                                '${time.hour} : ${time.minute} : ${time.second}';
+                            setState(() {});
+                          },
+                              currentTime: DateTime.now(),
+                              locale: LocaleType.en);
+                          setState(() {});
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    child: Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.access_time,
+                                          size: 18.0,
+                                          color: Colors.teal,
+                                        ),
+                                        Text(
+                                          " $_time",
+                                          style: TextStyle(
+                                              color: Colors.teal,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Text(
+                                "  Change",
+                                style: TextStyle(
+                                    color: Colors.teal,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        color: Colors.white,
+                      ),
+                      FlatButton(
+                          onPressed: () {
+                            DatePicker.showDatePicker(context,
+                                showTitleActions: true,
+                                minTime: DateTime(2000, 1, 1),
+                                maxTime: DateTime(2022, 12, 31),
+                                onChanged: (date) {
+                              print('change $date');
+                            }, onConfirm: (date) {
+                              print('confirm $date');
+                            },
+                                currentTime: DateTime.now(),
+                                locale: LocaleType.en);
+                          },
+                          child: Text(
+                            'Show DateTime Picker',
+                          )),
                       Container(
                         margin: EdgeInsets.only(top: 10),
                         width: 300,
