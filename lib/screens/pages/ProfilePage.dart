540515752e0607f9e77543b8a726e4158ea7aa39
Raadwan Masum <piraadwan@gmail.com>
2020-09-07 17:57:59 -0700

    create new profile page

---
+++ lib/screens/pages/ProfilePage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 12, top: 17, right: 12, bottom: 0),
+            width: 400,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Color(0xff3a3d41),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: 'Hey there, ',
+                                style: GoogleFonts.rubik(
+                                  color: Colors.white,
+                                  fontSize: 25.0,
+                                )),
+                            TextSpan(
+                              text: Provider.of<FirebaseUser>(context)
+                                  .displayName
+                                  .split(" ")[0],
+                              style: GoogleFonts.rubik(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 25.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '!',
+                              style: GoogleFonts.rubik(
+                                color: Colors.white,
+                                fontSize: 25.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
