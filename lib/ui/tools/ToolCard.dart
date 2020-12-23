a75f61195c6471b97af281def1d717d59c706c83
raad1masum <piraadwan@gmail.com>
2020-12-23 09:47:11 -0800

    redirect on tap tool container

--- lib/ui/tools/ToolCard.dart
+++ lib/ui/tools/ToolCard.dart
@@ -3,52 +3,63 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:OptixToolkit/ui/tools/ToolStatus.dart';
 import 'package:OptixToolkit/services/NavigationService.dart';
 
+import 'ReserveTool.dart';
+
 class ToolCard extends StatelessWidget {
   const ToolCard({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      width: 330,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xff26292c),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                RichText(
-                  text: TextSpan(
-                    style:
-                        GoogleFonts.rubik(color: Colors.white, fontSize: 20.0),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Band Saw",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0,
+    return GestureDetector(
+      onTap: () {
+        NavigationService.goTo(
+          PageRouteBuilder(
+            pageBuilder: (context, animation1, animation2) => ReserveTool(),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 10),
+        width: 330,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Color(0xff26292c),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: GoogleFonts.rubik(
+                          color: Colors.white, fontSize: 20.0),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Band Saw",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                ToolStatus(),
-                ToolStatus(),
-                ToolStatus(),
-                ToolStatus(),
-                ToolStatus()
-              ],
-            ),
-          ],
+                ],
+              ),
+              Row(
+                children: [
+                  ToolStatus(),
+                  ToolStatus(),
+                  ToolStatus(),
+                  ToolStatus(),
+                  ToolStatus()
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
