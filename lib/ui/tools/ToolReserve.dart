b8421348a8450e58528bc0db52139f53eba862e2
Aadit Gupta <spacegiant21@gmail.com>
2021-04-12 22:10:31 -0700

    admin check for add tool
    Co-authored-by: Raadwan Masum <piraadwan@gmail.com>
    Co-authored-by: Rohan Juneja <rohanj2006@gmail.com>

--- lib/ui/tools/ToolReserve.dart
+++ lib/ui/tools/ToolReserve.dart
@@ -1,11 +1,13 @@
 // Flutter imports:
 import 'package:OptixToolkit/services/NavigationService.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:OptixToolkit/services/database.dart';
 import 'package:OptixToolkit/ui/tools/ToolAdd.dart';
 
 // Package imports:
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import 'ToolReserveItem.dart';
 
@@ -84,18 +86,20 @@ class ToolReserve extends StatelessWidget {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-          NavigationService.goTo(
-            PageRouteBuilder(
-              pageBuilder: (context, animation1, animation2) => ToolAdd(),
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: Color(0xff159deb),
-      ),
+      floatingActionButton: (!!Provider.of<IdTokenResult>(context)
+              .claims['admin'])
+          ? FloatingActionButton(
+              onPressed: () {
+                NavigationService.goTo(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation1, animation2) => ToolAdd(),
+                  ),
+                );
+              },
+              child: const Icon(Icons.add),
+              backgroundColor: Color(0xff159deb),
+            )
+          : null,
     );
   }
 }
