180357d0ae9be5f27aa95fe4fd595eb7504c376a
Aadit Gupta <spacegiant21@gmail.com>
2021-09-05 14:22:26 -0700

    add a success alert

---
+++ lib/services/Good.dart
@@ -0,0 +1,71 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:google_fonts/google_fonts.dart';
+
+class Good extends StatelessWidget {
+  final String good;
+  const Good({Key key, this.good}) : super(key: key);
+
+  static Future<void> showGood(BuildContext context, String good) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return Good(good: good);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget doneButton = FlatButton(
+      child: Text(
+        "Done",
+        style: GoogleFonts.rubik(
+          fontWeight: FontWeight.bold,
+          color: Color(0xff159deb),
+        ),
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    return AlertDialog(
+      title: Text(
+        'Success!',
+        style: GoogleFonts.rubik(
+          fontWeight: FontWeight.bold,
+          color: Color(0xff159deb),
+          fontSize: 25.0,
+        ),
+      ),
+      backgroundColor: Color(0xff26292c),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '${good}',
+                    style: GoogleFonts.rubik(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        // editButton,
+        doneButton,
+      ],
+    );
+  }
+}
