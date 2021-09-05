8f086f6e82f2077cfbfc770bc97ffbe551fbf173
Aadit Gupta <spacegiant21@gmail.com>
2021-09-05 15:35:36 -0700

    add certified function

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -314,6 +314,31 @@ class Database {
     }
   }
 
+  static Future addCertifyRole(
+      IdTokenResult idToken, String uid, BuildContext context) async {
+    var client = http.Client();
+
+    Map data = {
+      'endpoint': 'certify-user',
+      'uid': uid,
+      'auth': idToken.token,
+    };
+
+    var body = json.encode(data);
+
+    var result = await client.post(Constants.SERVER_URL,
+        headers: {"Content-Type": "application/json"}, body: body);
+
+    if (result.statusCode == 200) {
+      return true;
+    } else {
+      print("ERROR");
+      print(result.body);
+      Alert.showAlert(context, jsonDecode(result.body)['err']);
+      return false;
+    }
+  }
+
   static Future reimbursement(
       IdTokenResult idToken,
       String personName,
