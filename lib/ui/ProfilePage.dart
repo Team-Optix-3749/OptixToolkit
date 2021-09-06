f95df0cf07408188986cd0f4a6e17b019fd56b9b
Raadwan Masum <piraadwan@gmail.com>
2021-09-06 14:31:04 -0700

    move user management up and make team management coming soon

--- lib/ui/ProfilePage.dart
+++ lib/ui/ProfilePage.dart
@@ -178,7 +178,8 @@ class profilePage extends StatelessWidget {
                                           child: RaisedButton(
                                             onPressed: () {
                                               Auth.sendPasswordResetEmail(
-                                                  emailController.text, context);
+                                                  emailController.text,
+                                                  context);
                                             },
                                             child: Text(
                                               'CHANGE PASSWORD',
@@ -197,118 +198,6 @@ class profilePage extends StatelessWidget {
                                 ),
                               ],
                             ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 12, top: 17, right: 12, bottom: 0),
-            width: 400,
-            height: MediaQuery.of(context).size.height * 0.20,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Color(0xff3a3d41),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: 'User ',
-                                style: GoogleFonts.rubik(
-                                  color: Colors.white,
-                                  fontSize: 25.0,
-                                )),
-                            TextSpan(
-                              text: 'Stats',
-                              style: GoogleFonts.rubik(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontSize: 25.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 5),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.89,
-                          margin: EdgeInsets.only(top: 5),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  RichText(
-                                    text: TextSpan(
-                                      children: <TextSpan>[
-                                        TextSpan(
-                                          text: 'Tools Reserved: ',
-                                          style: GoogleFonts.rubik(
-                                            color: Colors.white,
-                                            fontSize: 25.0,
-                                          ),
-                                        ),
-                                        TextSpan(
-                                          text: '100',
-                                          style: GoogleFonts.rubik(
-                                            color: Color(0xff159deb),
-                                            fontSize: 25.0,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  RichText(
-                                    text: TextSpan(
-                                      children: <TextSpan>[
-                                        TextSpan(
-                                          text: 'Parts Ordered: ',
-                                          style: GoogleFonts.rubik(
-                                            color: Colors.white,
-                                            fontSize: 25.0,
-                                          ),
-                                        ),
-                                        TextSpan(
-                                          text: '100',
-                                          style: GoogleFonts.rubik(
-                                            color: Color(0xff159deb),
-                                            fontSize: 25.0,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
                           ),
                         ),
                       ],
@@ -357,7 +246,7 @@ class profilePage extends StatelessWidget {
                         ),
                       ],
                     ),
-                    const SizedBox(height: 5),
+                    const SizedBox(height: 15),
                     Expanded(
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -410,6 +299,125 @@ class profilePage extends StatelessWidget {
                 ),
               ),
             ),
+          Container(
+            margin: EdgeInsets.only(left: 12, top: 17, right: 12, bottom: 0),
+            width: 400,
+            height: MediaQuery.of(context).size.height * 0.15,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Color(0xff3a3d41),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: 'User ',
+                                style: GoogleFonts.rubik(
+                                  color: Colors.white,
+                                  fontSize: 25.0,
+                                )),
+                            TextSpan(
+                              text: 'Stats',
+                              style: GoogleFonts.rubik(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 25.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.89,
+                          margin: EdgeInsets.only(top: 5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: 'Coming Soon!',
+                                          style: GoogleFonts.rubik(
+                                            color: Colors.white,
+                                            fontSize: 25.0,
+                                          ),
+                                        ),
+                                        // TextSpan(
+                                        //   text: 'Tools Reserved: ',
+                                        //   style: GoogleFonts.rubik(
+                                        //     color: Colors.white,
+                                        //     fontSize: 25.0,
+                                        //   ),
+                                        // ),
+                                        // TextSpan(
+                                        //   text: '100',
+                                        //   style: GoogleFonts.rubik(
+                                        //     color: Color(0xff159deb),
+                                        //     fontSize: 25.0,
+                                        //     fontWeight: FontWeight.bold,
+                                        //   ),
+                                        // ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              // const SizedBox(height: 5),
+                              // Row(
+                              //   mainAxisAlignment: MainAxisAlignment.center,
+                              //   children: [
+                              //     RichText(
+                              //       text: TextSpan(
+                              //         children: <TextSpan>[
+                              //           TextSpan(
+                              //             text: 'Parts Ordered: ',
+                              //             style: GoogleFonts.rubik(
+                              //               color: Colors.white,
+                              //               fontSize: 25.0,
+                              //             ),
+                              //           ),
+                              //           TextSpan(
+                              //             text: '100',
+                              //             style: GoogleFonts.rubik(
+                              //               color: Color(0xff159deb),
+                              //               fontSize: 25.0,
+                              //               fontWeight: FontWeight.bold,
+                              //             ),
+                              //           ),
+                              //         ],
+                              //       ),
+                              //     ),
+                              //   ],
+                              // ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
