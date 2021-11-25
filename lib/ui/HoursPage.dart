1b86e24fc48396c9e45de8c04a7080966450f801
Raadwan Masum <piraadwan@gmail.com>
2021-11-24 17:40:35 -0800

    create geofence

---
+++ lib/ui/HoursPage.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:location/location.dart';
+
+class hoursPage extends StatefulWidget {
+  const hoursPage({Key key}) : super(key: key);
+
+  @override
+  _hoursPageState createState() => _hoursPageState();
+}
+
+class _hoursPageState extends State<hoursPage> {
+  Location location = new Location();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ElevatedButton(
+            child: Text("Get Location"),
+            onPressed: () async {
+              var currentLocation = await location.getLocation();
+
+              final longitude = currentLocation.longitude;
+              final latitude = currentLocation.latitude;
+              final accuracy = currentLocation.accuracy;
+
+              print("Latitude: ${latitude}");
+              print("Longitude: ${longitude}");
+              print("Accuracy: ${accuracy} meters");
+            },
+          ),
+          ElevatedButton(
+            child: Text("Verify Geofence"),
+            onPressed: () async {
+              var currentLocation = await location.getLocation();
+
+              final longitude = currentLocation.longitude;
+              final latitude = currentLocation.latitude;
+
+              print("Latitude: ${latitude}");
+              print("Longitude: ${longitude}");
+
+              if (latitude > 37.42193608134135 &&
+                  latitude < 37.42206873207847 &&
+                  longitude > -122.08428558910641 &&
+                  longitude < -122.08391431794179) {
+                print("In range!");
+              } else {
+                print("Not in range :(");
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
